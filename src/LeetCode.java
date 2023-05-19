e5afa11769a1deaf5229a7dcb0ba36a7d5c12221
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-05-19 23:39:10 +0300

    Add mergeTwoLists exercise

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -169,4 +169,29 @@ public class LeetCode {
 
         return stack.isEmpty();
     }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode result = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                result.next = list1;
+                list1 = list1.next;
+            } else {
+                result.next = list2;
+                list2 = list2.next;
+            }
+
+            result = result.next;
+        }
+
+        if (list1 != null) {
+            result.next = list1;
+        } else {
+            result.next = list2;
+        }
+
+        return dummy.next;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 import java.lang.reflect.Array;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(LeetCode.romanToInt("III"));
-//
-//        String[] strs = {"flower","flow","flight"};
-//        System.out.println(LeetCode.longestCommonPrefix(strs));
 
-//        int[] nums = new int[]{3,2,4};
-//        int target = 6;
-//        int[] result = LeetCode.twoSum(nums, target);
-//        System.out.printf("[%d,%d]%n", result[0], result[1]);
-
-        System.out.println(LeetCode.isPalindrome(121));
     }
 }
